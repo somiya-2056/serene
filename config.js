d1d762331bae48bb29b495c1a1f3600f8500cd47
Somiya Aggarwal <somiyaaggarwal2056@gmail.com>
2026-09-05 22:41:15 +0530

    exposing config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__SERENE_CONFIG = {
+  SUPABASE_URL:      'https://wkxehbankbrbzcjjqyyb.supabase.co',  
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndreGVoYmFua2JyYnpjampxeXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMTIwODksImV4cCI6MjA5Mjg4ODA4OX0.NUAyql14-w7ot8HMDSDk2uZbYwc6SoRozao_BvpCeIs',
+  API_BASE:          'https://serene-n4sg.vercel.app'
+};
